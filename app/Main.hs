8c93ae3d4f078fd313aa31f28c9c08d4b9370e9a
Semion Shishkin <darkshish@gmail.com>
2020-04-13 15:33:40 +0300

    откат к началу

--- app/Main.hs
+++ app/Main.hs
@@ -1,55 +1,8 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Control.Exception          (try)
-import qualified Data.ByteString.Lazy.Char8 as L8
-import           Network.HTTP.Client        ( defaultManagerSettings
-                                            , newManager 
-                                            )
-import           Network.HTTP.Client.TLS    (tlsManagerSettings)
-import           Data.Aeson
-import qualified Data.ByteString.Char8      as S8
-import qualified Data.Yaml                  as Yaml
-import           Data.List
-import           Lib
-import           Network.HTTP.Simple
-import           Control.Monad
-import           Control.Monad.IO.Class    (liftIO)
-import qualified Data.ByteString           as S
-import qualified Data.Conduit.List         as CL
-import           Network.HTTP.Simple
-import           System.IO                 (stdout)
-
-{-
-main :: IO ()
-main = forever $ do
-    --url <- getLine
-    --loadSiteByURL url
-    url <- getLine
-    request <- parseRequest url
-    response <- httpLBS  request --"https://www.google.com"
-    print request
-    print response -}
-
-data Person = Person String Int
-instance ToJSON Person where
-    toJSON (Person name age) = object
-        [ "name" .= name
-        , "age"  .= age
-        ]
-
-people :: [Person]
-people = [Person "Alice" 30, Person "Bob" 35, Person "Charlie" 40]
+import    Lib
 
 
 main :: IO ()
 main = do
-    manager <- newManager tlsManagerSettings
-
-    let request = setRequestManager manager "https://httpbin.org/get"
-    response <- httpLBS request
-
-    putStrLn $ "The status code was: " ++
-               show (getResponseStatusCode response)
-    print $ getResponseHeader "Content-Type" response
-    L8.putStrLn $ getResponseBody response
+    someFunc
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,19 +1,11 @@
 module Lib
     ( someFunc
-    , loadSiteByURL
     ) where
 
-import           Network.HTTP.Conduit
-import qualified System.Directory as Dir
-import           System.IO
 
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-
-loadSiteByURL:: String -> IO ()
-loadSiteByURL url = do 
-    print url
 
   
